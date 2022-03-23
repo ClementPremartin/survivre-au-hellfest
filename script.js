22d4b02d6aab6ce73ea71ad4f2c4faf77523fe82
Lionel Manzetti <lionel.manzetti@gmail.com>
2022-03-23 16:30:13 +0100

    ajout fonctionnalité js changement ordre cartes selon selection

---
+++ script.js
@@ -0,0 +1,26 @@
+const plane = document.getElementById('plane');
+const carpool = document.getElementById('carpool');
+const bus = document.getElementById('bus');
+const train = document.getElementById('train');
+
+const select = document.getElementById('orderby');
+
+select.addEventListener('change', (event) => {
+    const value = event.target.value;
+    if (value == "price") {
+        plane.style.order = 4;
+        train.style.order = 2;
+        carpool.style.order = 1;
+        bus.style.order = 3;
+    } else if (value == "quickness") {
+        plane.style.order = 1;
+        train.style.order = 2;
+        carpool.style.order = 3;
+        bus.style.order = 4;
+    } else {
+        plane.style.order = 2;
+        train.style.order = 1;
+        carpool.style.order = 4;
+        bus.style.order = 3;
+    }
+});
