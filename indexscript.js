4a990f587f3c41eaac52aa146db4b337cbdaa9e8
LionelManzetti <100479719+LionelManzetti@users.noreply.github.com>
2022-03-25 14:25:56 +0100

    Update indexscript.js

--- indexscript.js
+++ indexscript.js
@@ -9,16 +9,16 @@ const spotify = document.querySelector('.embedded');
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        logo.src="https://raw.githubusercontent.com/ClementPremartin/survivre-au-hellfest/dev/images/LogoKawai.png"
-        logo2.src="https://raw.githubusercontent.com/ClementPremartin/survivre-au-hellfest/dev/images/LogoKawai.png"
+        logo.src="https://raw.githubusercontent.com/ClementPremartin/survivre-au-hellfest/dev/images/LogoKawai.png";
+        logo2.src="https://raw.githubusercontent.com/ClementPremartin/survivre-au-hellfest/dev/images/LogoKawai.png";
         headband.style.backgroundImage = 'none';
         headband.style.backgroundColor = 'var(--secondary-color)';
         spotify.innerHTML= `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/3hHmYc6mrl6NkmRW1ZwYvm?utm_source=generator"width="90%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>`
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
-        logo.src="https://raw.githubusercontent.com/ClementPremartin/survivre-au-hellfest/dev/images/logo-dark.png"
-        logo2.src="https://raw.githubusercontent.com/ClementPremartin/survivre-au-hellfest/dev/images/logo-dark.png"
+        logo.src="https://raw.githubusercontent.com/ClementPremartin/survivre-au-hellfest/dev/images/logo-dark.png";
+        logo2.src="https://raw.githubusercontent.com/ClementPremartin/survivre-au-hellfest/dev/images/logo-dark.png";
         spotify.innerHTML= `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DWZnlcbl9Edex?utm_source=generator" width="90%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>`
     }    
 }
